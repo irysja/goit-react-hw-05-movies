6fa7709e21ce3416395e49a4970135bb817c85ff
irysja <irysja.gyikova@gmail.com>
2023-10-01 17:18:28 +0300

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+//import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 //import { searchMovies } from '../api/Api'; // Import the searchMovies function
 import './Header.css';
 
 function Header() {
-  const [searchQuery, {/*setSearchQuery*/}] = useState('');
-const [isSearchOpen, {/*setIsSearchOpen*/}] = useState(false);
-
   return (
     <header className="header">
       <nav className="nav">
